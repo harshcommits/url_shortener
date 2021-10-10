5e7766957c4f0350e225dd858b06f2ddb7ef06ea
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2021-10-10 22:47:28 +0530

    added initial logic for shortURL

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect, url_for
+import os
+import random
 
-@app.route('/shorten')
-def shorten():
-    shortURL = open("url.txt", r)
-    for line in shortURL:
-        if 
+from url_short_test.url_short import short_url
+
+app = Flask(__name__)
+
+@app.route('/shorten/<string:longURL>')
+def shorten(longURL: str):
+    urlfile = open("url.txt", r)
+    if longURL in urlfile:
+        print ("value already present: " + urlfile[longURL])
+    else:
+        shortURL = url_for("short/" + random.randrange(100, 900))
+        with open("url.txt", a) as add_url:
+            add_url.write(shortURL)
