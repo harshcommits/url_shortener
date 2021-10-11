5a34b0812572dfca136c5ccd62387d53161ad787
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2021-10-11 09:41:43 +0530

    updated app for redirection

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ from flask import Flask, jsonify, request, redirect, url_for
 import os
 import random
 
-from url_short_test.url_short import short_url
-
 app = Flask(__name__)
 
 @app.route('/shorten/<string:longURL>')
 def shorten(longURL: str):
-    urlfile = open("url.txt", r)
+    urlfile = open("url.json", 'r')
     if longURL in urlfile:
         print ("value already present: " + urlfile[longURL])
     else:
         shortURL = url_for("short/" + random.randrange(100, 900))
-        with open("url.txt", a) as add_url:
+        with open("url.json", 'a') as add_url:
             add_url.write(shortURL)
+
+@app.route('/<shortURL>')
+def redirect_long(shortURL):
+    url_ref = open("url.json", 'r')
+    if shortURL in url_ref:
+        return redirect(url_ref.key[shortURL])
