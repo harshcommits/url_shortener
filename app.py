c9b1f482d7a6f6a8667d76297bc8c5322e3a3503
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2021-10-19 00:33:51 +0530

    syntax fixes and variable updates

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pyshorteners
 import json
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/check/<string:long_url>')
-def check(long_url: str):
+@app.route('/check', methods=['GET'])
+def check():
 
-    with open('urls.txt', 'r') as file_data:
-        file_json = json.load(file_data)
-        short_url = file_json.get(long_url)
+    long_url = request.args.get('long_url')
 
-    if long_url in file_json.keys():
-        return jsonify(message="Short URL: " + short_url)
+    if os.stat('urls.txt').st_size != 0:
+        with open('urls.txt', 'r') as file_data:
+            file_json = json.load(file_data)
+            short_url = file_json.get(long_url)
+
+            if long_url in file_json.keys():
+                return jsonify(message="Short URL: " + short_url)
+            else:
+                return jsonify(message='Data not present')
+
     else:
-        return jsonify(message='Data not present')
+        return jsonify(message="The file is not readable")
 
 
-@app.route('/shorten/<string:long_url>', methods=['POST'])
-def shorten(long_url: str):
+@app.route('/shorten', methods=['POST'])
+def shorten():
 
+    long_url = request.args.get('long_url')
+    short_url = pyshorteners.Shortener().tinyurl.short(long_url)
+    new_entry = {long_url: short_url}
     with open('urls.txt', 'a') as file:
-        file_add = json.dumps(file)
-        short_url = pyshorteners.Shortener.tinyurl.short(long_url)
-        file_add.write({long_url: short_url})
+        json.dump(new_entry, file)
+        return jsonify(message="New short URL created: " + short_url )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(DEBUG=True)
