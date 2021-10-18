8fbc439a20c5e824b9847ce27175bd8711ffaceb
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2021-10-18 23:37:12 +0530

    updated logic and requirements.txt

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-from flask import Flask, jsonify, request, redirect, url_for
-import os
+from flask import Flask, jsonify
 import pyshorteners
 import json
-import random
 
 app = Flask(__name__)
 
-@app.route('/shorten/<string:longURL>')
-def shorten(longURL: str):
-    urlfile = json.loads(open("url.json", 'r'))
-    if longURL in urlfile:
-        print ("value already present: " + urlfile[longURL])
-    else:
-        shortURL = pyshorteners.Shortener().tinyURL.short(longURL)
-        with open("url.json", 'a') as add_url:
-            add_url.write(shortURL)
 
-@app.route('/<shortURL>')
-def redirect_long(shortURL):
-    url_ref = open("url.json", 'r')
-    if shortURL in url_ref:
-        return redirect(url_ref.key[shortURL])
+@app.route('/check/<string:long_url>')
+def check(long_url: str):
+
+    with open('urls.txt', 'r') as file_data:
+        file_json = json.load(file_data)
+        short_url = file_json.get(long_url)
+
+    if long_url in file_json.keys():
+        return jsonify(message="Short URL: " + short_url)
+    else:
+        return jsonify(message='Data not present')
+
+
+@app.route('/shorten/<string:long_url>')
+def shorten(long_url: str):
+
+    with open('urls.txt', 'a') as file:
+        file_add = json.dumps(file)
+        short_url = pyshorteners.Shortener.tinyurl.short(long_url)
+        file_add.write({long_url: short_url})
+
+
+if __name__ == '__main__':
+    app.run()
