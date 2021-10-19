67456403e934c0e192a2ac63f85fe16f5a92546b
Harsh Jha <harshjha@Harshs-MacBook-Air.local>
2021-10-19 23:45:14 +0530

    added url shortening logic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, request
-import pyshorteners
+from flask import Flask, jsonify, request, redirect
+import random
+import string
 import json
 import os
 
@@ -14,10 +15,10 @@ def check():
     if os.stat('urls.txt').st_size != 0:
         with open('urls.txt', 'r') as file_data:
             file_json = json.load(file_data)
-            short_url = file_json.get(long_url)
 
-            if long_url in file_json.keys():
-                return jsonify(message="Short URL: " + short_url)
+            for long, short in file_json.items():
+                if long == long_url:
+                    return jsonify(message="Short URL: " + short + "already exists")
             else:
                 return jsonify(message='Data not present')
 
@@ -29,17 +30,35 @@ def check():
 def shorten():
 
     long_url = request.args.get('long_url')
-    short_url = pyshorteners.Shortener().tinyurl.short(long_url)
+
+    content = string.ascii_lowercase + string.digits
+    short_url = random.choices(content, k=6)
+    short_url = "".join(short_url)
 
     with open('urls.txt', 'r') as file:
         file_json = json.load(file)
-        file_json[long_url] = short_url
+        file_json[short_url] = long_url
 
     with open('urls.txt', 'w') as file:
-        #   file_json = {long_url: short_url}
+        #   file_json = {short_url: long_url}
         json.dump(file_json, file)
-        return jsonify(message="New short URL created: " + short_url )
+        return jsonify(message="New short URL created: " + short_url)
+
+
+@app.route('/<short_url>')
+def get_short(short_url):
+
+    with open('urls.txt', 'r') as file:
+        file_json = json.load(file)
+
+        if short_url in file_json.keys():
+            long_url = file_json.get(short_url)
+            print("long URL is: " + long_url)
+            return redirect(long_url)
+
+        # else:
+            # return jsonify(message="URL does not exist")
 
 
 if __name__ == '__main__':
-    app.run(DEBUG=True)
+    app.run()
